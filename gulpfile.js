e2c9638ab2a6837305881cddacc09a639f5df61c
Richard Kalehoff <richardkalehoff@gmail.com>
2016-04-19 23:10:50 -0700

    add gulp-inline-source package

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var inlinesource = require('gulp-inline-source');
 
 gulp.task('copy-html', function() {
     gulp.src('app/index.html')
+        .pipe(inlinesource())
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('./dist'));
 });
